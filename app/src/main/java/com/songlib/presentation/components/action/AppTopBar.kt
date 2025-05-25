2eba4fdd28dc70292990a9c1cfe2c587b68d7779
Siro Daves <sirodevske@gmail.com>
2025-05-26 01:59:27 +0300

    toast notification

--- app/src/main/java/com/songlib/presentation/components/action/AppTopBar.kt
+++ app/src/main/java/com/songlib/presentation/components/action/AppTopBar.kt
@@ -1,12 +1,20 @@
 package com.songlib.presentation.components.action
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.songlib.presentation.theme.ThemeColors
@@ -50,27 +58,43 @@ fun SearchTopBar(
     Surface(shadowElevation = 3.dp) {
         TopAppBar(
             title = {
-                TextField(
-                    value = query,
-                    onValueChange = onQueryChange,
-                    modifier = Modifier.fillMaxWidth(),
-                    placeholder = { Text("Search songs") },
-                    singleLine = true,
-                    trailingIcon = {
-                        IconButton(onClick = onClose) {
-                            Icon(Icons.Filled.Close, contentDescription = "Close")
-                        }
-                    },
-                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 15.dp, vertical = 5.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    TextField(
+                        value = query,
+                        onValueChange = onQueryChange,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                            .clip(RoundedCornerShape(25.dp))
+                            .background(MaterialTheme.colorScheme.surface),
+                        placeholder = { Text("Search for songs") },
+                        singleLine = true,
+                        trailingIcon = {
+                            IconButton(onClick = onClose) {
+                                Icon(Icons.Filled.Close, contentDescription = "Close")
+                            }
+                        },
+//                        colors = TextFieldDefaults.TextFieldColors(
+//                            containerColor = Color.White,
+//                            unfocusedIndicatorColor = Color.Transparent,
+//                            focusedIndicatorColor = Color.Transparent,
+//                            disabledIndicatorColor = Color.Transparent
+//                        ),
+                        shape = RoundedCornerShape(24.dp)
+                    )
+                }
             },
             navigationIcon = {},
             actions = {},
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = ThemeColors.primary,
-                titleContentColor = Color.White,
-                actionIconContentColor = Color.White,
-                navigationIconContentColor = Color.White
-            ),
+                titleContentColor = Color.White
+            )
         )
     }
 }
